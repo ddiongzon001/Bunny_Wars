2a44faa3c6efdb93492142941a3dfcc461a7ffa6
Dealan Diongzon <dealan.diongzon@gmail.com>
2019-06-30 10:04:30 -0400

    created a very bare skeleton of how the HTML page will look like (includes the different sections and a button and also made sure that bootstrap, css file and js file all link properly to the html page

---
+++ javascript/game.js
@@ -0,0 +1,14 @@
+//Define variables
+    //Characters
+    
+
+    //HTML Display variables
+    var mainCharDisplay = document.getElementById("main-char");
+    
+//Define Functions
+
+//MAIN SECTION OF GAME
+    mainCharDisplay.textContent = "hello!";
+
+
+
