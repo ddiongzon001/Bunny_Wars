51fea26e3ac96cdac6b3009f434fb7b2bf51c4ce
Dealan Diongzon <dealan.diongzon@gmail.com>
2019-06-30 15:57:37 -0400

    added images to the character choice section

--- javascript/game.js
+++ javascript/game.js
@@ -3,7 +3,7 @@
     //Characters
     var characters = {
         name: ["Bugs Bunny", "Lola Bunny", "Buster Bunny", "Babs Bunny"],
-        img: ["img1", "img2", "img3","img4"],
+        img: ["../Bunny_Wars/assets/images/bugs-bunny.jpg", "../Bunny_Wars/assets/images/Lola-Bunny.png", "../Bunny_Wars/assets/images/buster-bunny.jpg","../Bunny_Wars/assets/images/babs-bunny.jpg"],
         hp: [120,115,110,105]
     }
 
@@ -14,7 +14,12 @@
     var char2NameDisplay = $('#character2-name');
     var char3NameDisplay = $('#character3-name');
     var char4NameDisplay = $('#character4-name');
-    //IMAGES HERE
+
+    var char1PicDisplay = $('#character1-picture');
+    var char2PicDisplay = $('#character2-picture');
+    var char3PicDisplay = $('#character3-picture');
+    var char4PicDisplay = $('#character4-picture');
+
     var char1HPDisplay = $('#character1-HP');
     var char2HPDisplay = $('#character2-HP');
     var char3HPDisplay = $('#character3-HP');
@@ -29,6 +34,15 @@
         char3NameDisplay.text(characters.name[2]);
         char4NameDisplay.text(characters.name[3]);
         //IMAGES HERE
+        char1PicDisplay.attr('src', characters.img[0]);
+        char1PicDisplay.width('100px');
+        char2PicDisplay.attr('src', characters.img[1]);
+        char2PicDisplay.width('100px');
+        char3PicDisplay.attr('src', characters.img[2]);
+        char3PicDisplay.width('100px');
+        char4PicDisplay.attr('src', characters.img[3]);
+        char4PicDisplay.width('100px');
+
         char1HPDisplay.text(characters.hp[0]);
         char2HPDisplay.text(characters.hp[1]);
         char3HPDisplay.text(characters.hp[2]);
